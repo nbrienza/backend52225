616d3c59af72236cd4a1d294b5a62cb99a10bdc7
nbrienza <nicobrienza@gmail.com>
2023-05-30 19:39:31 -0300

    entrega 3

--- src/app.js
+++ src/app.js
@@ -12,3 +12,31 @@ app.use(express.urlencoded({
 app.get ("/kepio", (req, res)=>{
     return res.json({kepio:"awper"})
 })
+
+
+// Cargar los productos desde el archivo
+const productsData = JSON.parse(fs.readFileSync('productsRouter.json'));
+
+// Ruta para obtener todos los productos o un número limitado de ellos
+app.get('/products', (req, res) => {
+  const { limit } = req.query;
+  
+  if (limit) {
+    const limitedProducts = productsData.slice(0, limit);
+    res.json(limitedProducts);
+  } else {
+    res.json(productsData);
+  }
+});
+
+// Ruta para obtener un producto por su ID
+app.get('./productsRouters.js/:id', (req, res) => {
+  const { id } = req.params;
+  const product = productsData.find(p => p.id === parseInt(id));
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: 'El producto no existe' });
+  }
+});
